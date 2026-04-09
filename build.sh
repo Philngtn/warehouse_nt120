137b2a595b304a630eefdd57709712ebcb11caca
Phil Nguyen <philngtn@gmail.com>
2026-04-09 00:36:13 -0400

    Fix UI

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Generates config.js from environment variables (used by Vercel build)
-cat > config.js <<EOF
-const VITE_SUPABASE_URL = '${VITE_SUPABASE_URL}';
-const VITE_SUPABASE_ANON_KEY = '${VITE_SUPABASE_ANON_KEY}';
-EOF
-echo "config.js generated"
